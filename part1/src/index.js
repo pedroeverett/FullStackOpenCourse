15e65d49053a8bb529a15c97963d47619fa9efab
Pedro Everett <pedroeverett@iMac-de-Pedro.local>
2019-09-07 12:32:19 +0100

    courseinfo part 1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
+
+const Hello = (props) => {
+    return (
+        <div>
+            <p>Hello {props.name}, you are {props.age} years old.</p>
+        </div>
+    )
+}
+
+const App = () => {
+    const name = 'Peter';
+    const age = 10;
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name={'Pedro'} age={10 + 5}/>
+            <Hello name={name} age={age} />
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
